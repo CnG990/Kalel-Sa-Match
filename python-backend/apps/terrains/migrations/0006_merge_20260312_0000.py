99d40ed0cb75f57a15586295cbc37dd600bf5650
CnG990 <comptenetflixcm1@gmail.com>
2026-03-12 14:33:45 +0000

    Merge migrations 0005_* for nombre_joueurs

---
+++ python-backend/apps/terrains/migrations/0006_merge_20260312_0000.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("terrains", "0005_planabonnement_reduction_percent"),
+        ("terrains", "0005_alter_terrainsynthetiquesdakar_nombre_joueurs"),
+    ]
+
+    operations = []
